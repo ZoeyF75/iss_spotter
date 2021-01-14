ea09b7f65747914b9b0a1e69506c3e55e1251038
Zoey Farago <zf.zoodle@gmail.com>
2021-01-14 22:38:23 +0000

    Part two find coordinates using API and IP address

---
+++ iss.js
@@ -0,0 +1,37 @@
+const request = require("request");
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api64.ipify.org?format=json', (error, response, body) => {
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    error ? callback(error, null) : callback(null, JSON.parse(body).ip);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates by IP. Response: ${body}`;
+      callback(Error(msg), null, null);
+      return;
+    }
+    error ? callback(error, null, null) : callback(null, JSON.parse(body).latitude, JSON.parse(body).longitude);
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
